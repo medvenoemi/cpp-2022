c037a7a43e564a933982d127325f76fc70bc2a7c
Noemi <medve.noemi.nora@student.ms.sapientia.ro>
2022-12-04 11:34:48 +0200

    Lab11

--- lab09/Ceg.cpp
+++ lab09/Ceg.cpp
@@ -4,13 +4,48 @@
 
 #include "Ceg.h"
 
-Ceg::Ceg(const vector<Alkalmazott *> &alkalmazottak) : alkalmazottak(alkalmazottak) {}
-
-
 void Ceg::addAlklamazott(Alkalmazott *alkalmazott) {
     alkalmazottak.push_back(alkalmazott);
 }
 
-void Ceg::addAlkalmazottToManager(Manager *manager, Alkalmazott *alkalmazott) {
+void Ceg::deleteAlkalmazottById(int id) {
+    auto index = find_if(this->alkalmazottak.begin(),this->alkalmazottak.end(), [&id](Alkalmazott *szemely){
+        return szemely->getId() == id;
+    });
 
+    this->alkalmazottak.erase(index);
+}
+
+void Ceg::addAlkalmazottToManager(Manager *manager, Alkalmazott *alkalmazott) {
+    for_each(alkalmazottak.begin(), alkalmazottak.end(), [alkalmazott, manager](Alkalmazott *alkalmazott1) {
+        if (alkalmazott1 == manager) {
+            manager->addAlkalmazott(alkalmazott);
+        }
+    });
+}
+
+void Ceg::deleteAlkalmazottFromManager(Manager *manager, Alkalmazott *alkalmazott) {
+    for_each(alkalmazottak.begin(), alkalmazottak.end(), [alkalmazott, manager](Alkalmazott *alkalmazott1) {
+        if (alkalmazott1 == manager) {
+            manager->deleteAlkalmazott(alkalmazott);
+        }
+    });
+}
+
+void Ceg::printAll(std::ostream &os) {
+    os << "Ceg:" << endl;
+    for_each(alkalmazottak.begin(), alkalmazottak.end(), [&os](Alkalmazott *alkalmazott) {
+        alkalmazott->print(os);
+    });
+}
+
+void Ceg::printManagers(std::ostream &os) {
+    os << "Managers:" << endl;
+    for_each(alkalmazottak.begin(), alkalmazottak.end(), [&os](Alkalmazott *alkalmazott) {
+        Manager *tempManager;
+        tempManager = dynamic_cast<Manager *>(alkalmazott);
+        if (tempManager != nullptr) {
+            alkalmazott->print(os);
+        }
+    });
 }
---
+++ lab11/Index.h
@@ -0,0 +1,26 @@
+//
+// Created by Noemi on 11/28/2022.
+//
+
+#ifndef CPP_2022_INDEX_H
+#define CPP_2022_INDEX_H
+
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Index {
+
+    map<string, vector<int>> index;
+
+public:
+    Index(const string& filename);
+    void printIndex(ostream &out =cout) const;
+    void printIndexWords(ostream &out =cout) const;
+    const vector<int>& getWordOccurencies(const string& key);
+};
+
+
+#endif //CPP_2022_INDEX_H
