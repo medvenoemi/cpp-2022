3756c7e98dbdf62ed45909392e27181c34d163f2
Noemi <medve.noemi.nora@student.ms.sapientia.ro>
2022-12-12 20:34:26 +0200

    Lab 13

---
+++ lab13/myifstream.h
@@ -0,0 +1,26 @@
+//
+// Created by Noemi on 12/12/2022.
+//
+
+#ifndef CPP_2022_MYIFSTREAM_H
+#define CPP_2022_MYIFSTREAM_H
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class myifstream: public ifstream{
+
+public:
+    myifstream(const string filename);
+
+    ~myifstream() override;
+
+    char operator[](long index);
+
+    long filesize();
+};
+
+
+#endif //CPP_2022_MYIFSTREAM_H
